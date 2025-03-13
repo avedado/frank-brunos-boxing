034a304c77bc2d9e69b7f7c4c21c4bb6a63c7221
avedado <mariaalonsoteis@gmail.com>
2025-03-13 12:20:28 +0100

    crear proyecto

---
+++ src/main/java/edu/estatuas/ScoreCard.java
@@ -0,0 +1,12 @@
+package edu.estatuas;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class ScoreCard {
+
+
+
+
+
+
+}
