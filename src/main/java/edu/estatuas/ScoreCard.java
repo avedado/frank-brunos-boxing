323cd8dad822a1ad8a3dd2313d2d66f3713703c1
avedado <mariaalonsoteis@gmail.com>
2025-03-13 14:09:49 +0100

    Clase ScoreCard

--- src/main/java/edu/estatuas/ScoreCard.java
+++ src/main/java/edu/estatuas/ScoreCard.java
@@ -3,10 +3,29 @@ package edu.estatuas;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class ScoreCard {
+    private final String color;
+    private String redCorner="";
+    private String blueCorner="";
+    private String judgeScoreCard;
 
+    public ScoreCard (String color) {
+        this.color = color;
+    }
 
+    public void setRCorner(String boxerName) {
+        this.redCorner = boxerName;
+    }
 
+    public void setBCorner(String boxerName) {
+        this.blueCorner = boxerName;
+    }
 
+   // public byte getNumRounds(){
+    //    return
+    //}
 
+    public int getRedBoxerFinalScore() {
+        return Integer.parseInt(redCorner);
+    }
 
 }
