b606f96f8f30e1c4209ee0f0d631f6d8458d3030
arianaborrajo <borrajoarias@gmail.com>
2025-03-14 13:39:38 +0100

    Clase PointsDeducted

---
+++ src/main/java/edu/estatuas/PointsDeducted.java
@@ -0,0 +1,37 @@
+package edu.estatuas;
+
+public class PointsDeducted {
+                private String roundScore="";
+                public byte redBoxerScore;
+                public byte blueBoxerScore;
+
+                PointsDeducted(String roundScore) {
+                    if(this.roundScore = roundScore.replaceAll("\\s", "");
+                    this.parseBoxerRoundScore();
+                }
+
+                public void boxerRoundScore() {
+                    return this.roundScore
+                }
+
+                private byte parseComaBlue(String score) {
+                  return blueBoxerScore;
+                }
+
+                private byte parseComaRed(String score) {
+                    return redBoxerScore;
+                }
+
+                public byte getRedBoxerScore() {
+                    return redBoxerScore;
+                }
+
+                public byte getBlueBoxerScore() {
+                    return blueBoxerScore;
+                }
+
+                public String toString(){
+                    return this.getRedBoxerScore() + " - " + this.getBlueBoxerScore();
+                }
+
+}
