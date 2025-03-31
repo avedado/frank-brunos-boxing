e8aae7723cb3cb36db68b86c5fee9b41c590089e
avedado <mariaalonsoteis@gmail.com>
2025-03-31 17:27:04 +0200

    refactor: Clase PointsDeducted

--- src/main/java/edu/estatuas/PointsDeducted.java
+++ src/main/java/edu/estatuas/PointsDeducted.java
@@ -7,15 +7,26 @@ public class PointsDeducted implements Round {
 
     PointsDeducted(String roundScore) {
         if(this.roundScore == roundScore.replaceAll("\\s", ""));
-        this.boxerRoundScore();
+        this.parseBoxerRoundScore();
     }
 
-    public String boxerRoundScore() {
-        String score=roundScore.replace("\s", "");
-        int position = score.indexOf("-");
-        this.redBoxerScore = (byte) Integer.parseInt(score.substring(0, position));
-        this.blueBoxerScore = (byte) Integer.parseInt(score.substring(position + 1));
-        if (roundScore.replace(",1", "" || "1,", "");
+    String getRoundScore() {
+        return this.roundScore;
+    }
+
+    private void parseBoxerRoundScore() {
+
+        String[] scores = getRoundScore().split("-", 2);
+        String redBoxerRoundScore = scores[0];
+        String blueBoxerRoundScore = scores[1];
+
+        if (redBoxerRoundScore.indexOf(',') == -1) {
+            this.redBoxerScore = Byte.parseByte(redBoxerRoundScore);
+            this.blueBoxerScore = this.parseComaBlue(blueBoxerRoundScore);
+        } else {
+            this.blueBoxerScore = Byte.parseByte(blueBoxerRoundScore);
+            this.redBoxerScore = this.parseComaRed(redBoxerRoundScore);
+        }
     }
 
     private byte parseComaBlue(String score) {
